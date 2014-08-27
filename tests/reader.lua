c2d7f8cc3d90a52c714e040e2e2a98719e5b2b14
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-08-27 10:06:20 -0400

    add more test code from stackoverflow

--- tests/reader.lua
+++ tests/reader.lua
@@ -9,7 +9,20 @@ test([[
 (-42) (asdf) ((foo)) ((foo) bar ((fuzz)))
 ]])
 test([[
-'42 '`''(asdf)
+'42 '`````''(asdf)
+]])
+test([[
+(def foobar (1 2 3))
+]])
+
+-- http://stackoverflow.com/questions/3683388/given-the-following-lisp-eval-function-what-is-required-to-add-defmacro
+test([[
+((eq (caar e) 'macro)
+     (cond
+      ((eq (cadar e) 'lambda)
+       (eval. (eval. (car (cdddar e))
+                     (cons (list. (car (caddar e)) (cadr e)) a))
+              a))))
 ]])
 
 return true
