1ac81936df42023924d71f652cd5734ad4d09d24
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-09-14 15:57:41 -0400

    move to cons_cell.lua and remove util.lua

--- core/cons_cell.lua
+++ core/cons_cell.lua
@@ -10,3 +10,12 @@ end
 function tsukuyomi.is_cons_cell(datum)
   return getmetatable(datum) == kCellTag
 end
+
+function tsukuyomi.cons_to_lua_array(datum)
+  local arr = {}
+  while datum and datum[1] do
+    table.insert(arr, datum[1])
+    datum = datum[2]
+  end
+  return arr
+end
--- core/util.lua
+++
@@ -1,8 +0,0 @@
-function tsukuyomi.cons_to_lua_array(datum)
-  local arr = {}
-  while datum and datum[1] do
-    table.insert(arr, datum[1])
-    datum = datum[2]
-  end
-  return arr
-end
