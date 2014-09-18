8f1aeab02111a92027720d595a67c13f2de5d0c2
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-09-17 22:47:08 -0400

    add a cache for namespace fetching

--- core/namespace.lua
+++ core/namespace.lua
@@ -1,9 +1,14 @@
 local tsukuyomi = tsukuyomi
 local util = require('tsukuyomi.thirdparty.util')
 
+local namespace_cache = setmetatable({}, {__mode = 'kv'})
+
 -- namespaces are just a series of table starting from the global table
 -- therefore tsukuyomi.core == _G.tsukuyomi.core == _G["tsukuyomi"]["core"]
 function tsukuyomi.get_namespace(ns)
+  local t = namespace_cache[ns]
+  if t then return t end
+  
   local tokens = util.split(ns, '.')
   assert(#tokens >= 1)
   -- support strict.lua
@@ -13,9 +18,12 @@ function tsukuyomi.get_namespace(ns)
   local t = _G
   for i = 1, #tokens do
     local ns_chunk = tokens[i]
-    if t[ns_chunk] == nil then t[ns_chunk] = {} end
+    if t[ns_chunk] == nil then
+      t[ns_chunk] = {}
+    end
     t = t[ns_chunk]
   end
+  namespace_cache[ns] = t
   return t
 end
 
