27189c56da10649b23697ccfbeb64ae9b5470f59
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-09-28 23:45:43 -0400

    rework how namespaces conceptually used so it is now an object and is clearer

--- core/namespace.lua
+++ core/namespace.lua
@@ -1,18 +1,11 @@
 local tsukuyomi = tsukuyomi
 local util = require('tsukuyomi.thirdparty.util')
 
-local namespace_cache = setmetatable({}, {__mode = 'kv'})
-
--- namespaces are just a series of table starting from the global table
--- therefore tsukuyomi.core == _G.tsukuyomi.core == _G["tsukuyomi"]["core"]
-function tsukuyomi.get_namespace(ns)
-  local t = namespace_cache[ns]
-  if t then return t end
-  
-  local tokens = util.split(ns, '.')
+local function create_or_get_space(name)
+  local tokens = util.split(name, '.')
   assert(#tokens >= 1)
   -- support strict.lua
-  if _G.__STRICT then
+  if __STRICT then
     global(tokens[1])
   end
   local t = _G
@@ -23,25 +16,48 @@ function tsukuyomi.get_namespace(ns)
     end
     t = t[ns_chunk]
   end
-  namespace_cache[ns] = t
   return t
 end
 
--- the namespace argument is just a string like "tsukuyomi.core"
-function tsukuyomi.set_active_namespace(namespace)
-  -- TODO: mimic clojure / python and have a way to import symbols from other namespaces
-  -- so you import math / other libraries
-  local ns = {}
-  local mt = {}
-  function mt.__index(t, symbol_name)
-    local imported_ns = rawget(t, symbol_name)
-    if imported_ns == nil then
-      return namespace
-    else
-      return imported_ns
-    end
+local Namespace = {}
+Namespace.__index = Namespace
+
+function Namespace.new(name)
+  local t = {}
+  t.name = name
+  t.space = create_or_get_space(name)
+  return setmetatable(t, Namespace)
+end
+
+function Namespace:bind_symbol(symbol)
+  assert(symbol.namespace == nil)
+  -- TODO: mimic clojure / python and have a way to import symbols from other
+  -- namespaces so you import math / other libraries
+  return self.name
+end
+
+local namespace_cache = {}
+
+function tsukuyomi.get_namespace(name)
+  local ns
+
+  if namespace_cache[name] then
+    ns = namespace_cache[name]
+  else
+    ns = Namespace.new(name)
+    namespace_cache[name] = ns
   end
-  setmetatable(ns, mt)
-  tsukuyomi.get_namespace('tsukuyomi.core')['*ns*'] = ns
+
+  return ns
+end
+
+function tsukuyomi.get_namespace_space(name)
+  return tsukuyomi.get_namespace(name).space
+end
+
+-- the namespace argument is just a string like "tsukuyomi.core"
+function tsukuyomi.set_active_namespace(name)
+  local ns = tsukuyomi.get_namespace(name)
+  tsukuyomi.get_namespace('tsukuyomi.core').space['*ns*'] = ns
   return ns
 end
