283ef19ef04ff154162edf1d9c3ad4d1f6f2d419
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-01 22:49:58 -0400

    simple PushbackReader based on Clojure

---
+++ lang/PushbackReader.lua
@@ -0,0 +1,33 @@
+local tsukuyomi_lang = tsukuyomi.lang.Namespace.GetNamespaceSpace('tsukuyomi.lang')
+
+local PushbackReader = {}
+tsukuyomi_lang.PushbackReader = PushbackReader
+PushbackReader.__index = PushbackReader
+
+function PushbackReader.new(text)
+  local t = {}
+  t.text = text
+  t.i = 1
+  t.push_back_buffer = {}
+  return setmetatable(t, PushbackReader)
+end
+
+function PushbackReader:read()
+  if #self.push_back_buffer > 0 then
+    return table.remove(self.push_back_buffer)
+  else
+    local i = self.i
+    local ch = self.text:sub(i, i)
+    -- EOF
+    if ch == '' then
+      return ch
+    end
+
+    self.i = i + 1
+    return ch
+  end
+end
+
+function PushbackReader:unread(ch)
+  table.insert(self.push_back_buffer, ch)
+end
