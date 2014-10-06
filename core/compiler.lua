a01fd95ead3dadbf11de55e21785fb9ef5ff572d
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-05 20:40:49 -0400

    put Compiler in proper namespace

--- core/compiler.lua
+++ core/compiler.lua
@@ -1,23 +1,23 @@
+local tsukuyomi = tsukuyomi
+local Compiler = tsukuyomi.lang.Namespace.GetNamespaceSpace('tsukuyomi.lang.Compiler')
+
 local log = io.open('tsukuyomi/tests/compiler.out.txt', 'w')
 if not log then assert(false) end
 
-function tsukuyomi.compile(datum)
-  log:write('compiling:\n')
+function Compiler.compile(datum)
+  log:write('*** Lisp ***\n\n')
   log:write(tsukuyomi.print(datum))
-  log:write('\n')
-  log:write('\n')
+  log:write('\n\n')
 
-  local list = tsukuyomi.compile_to_ir(datum)
-  log:write('IR:\n')
-  log:write(tsukuyomi._debug_ir(list))
-  log:write('\n')
-  log:write('\n')
+  local list = Compiler.compile_to_ir(datum)
+  log:write('*** IR ***\n\n')
+  log:write(Compiler._debug_ir(list))
+  log:write('\n\n')
 
-  local source_code = tsukuyomi.compile_to_lua(list)
-  log:write('Lua source code:\n')
+  local source_code = Compiler.compile_to_lua(list)
+  log:write('*** Lua source code ***\n\n')
   log:write(source_code)
-  log:write('\n')
-  log:write('\n')
+  log:write('\n\n')
 
   log:write('********************************************************************************\n\n')
   return source_code
