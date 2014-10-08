00d62435e0a602d984883ef3fbb9690001cc2ff0
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-07 21:48:02 -0400

    add a function to describe what is being evaluated

--- core/eval.lua
+++ core/eval.lua
@@ -2,10 +2,26 @@ local tsukuyomi = tsukuyomi
 local tsukuyomi_core = tsukuyomi.core
 local PushbackReader = tsukuyomi.lang.PushbackReader
 local Compiler = tsukuyomi.lang.Namespace.GetNamespaceSpace('tsukuyomi.lang.Compiler')
+local PersistentList = tsukuyomi.lang.PersistentList
+local Symbol = tsukuyomi.lang.Symbol
 
 tsukuyomi_core['eval'] = tsukuyomi.lang.Function.new()
 tsukuyomi_core['eval'][1] = function (form)
   assert(false)
+end
+
+local def_symbol = Symbol.intern('def')
+
+local function describe(datum)
+  local info
+  if getmetatable(datum) == PersistentList then
+    if datum:first() == def_symbol then
+      local symbol = datum:rest():first()
+      symbol = tsukuyomi.core['*ns*']:bind_symbol(symbol)
+      info = tostring(symbol)
+    end
+  end
+  return info
 end
 
 tsukuyomi_core['load-file'] = tsukuyomi.lang.Function.new()
@@ -18,7 +34,7 @@ tsukuyomi_core['load-file'][1] = function (name)
   local datum = tsukuyomi_core.read[1](r)
   while datum do
     local lua_code = Compiler.compile(datum)
-    local chunk, err = loadstring(lua_code)
+    local chunk, err = loadstring(lua_code, describe(datum))
     if err then
       io.stderr:write(err)
       io.stderr:write('\n')
