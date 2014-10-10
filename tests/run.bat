07300cc95f8802ef2eb04093df83061f5200f2b0
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-10 00:53:01 -0400

    add bytecode

--- tests/run.bat
+++ tests/run.bat
@@ -9,6 +9,8 @@ echo 51
 luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 51
 echo 70
 luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 70
+echo 72
+luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 72
 
 REM luajit-2.0\src\luajit.exe tsukuyomi\tests\run_all_tests.lua
 luajit-2.0\src\luajit.exe tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\test_output.log
