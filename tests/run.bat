f627f19f9f217b5dcc1b2fd027ba8f48551e8ac4
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-12 22:43:59 -0400

    use only one call to print bytecode names

--- tests/run.bat
+++ tests/run.bat
@@ -3,14 +3,7 @@ cls
 pushd %~dp0
 cd ..\..
 
-echo 50
-luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 50
-echo 51
-luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 51
-echo 70
-luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 70
-echo 72
-luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 72
+luajit-2.0\src\luajit.exe tsukuyomi\thirdparty\bcname.lua 50 51 70 72
 
 REM luajit-2.0\src\luajit.exe tsukuyomi\tests\run_all_tests.lua
 luajit-2.0\src\luajit.exe tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\test_output.log
