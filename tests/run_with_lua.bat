d0471757f8af91e95d36274c72601db56304acac
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-28 20:29:53 -0400

    switch to Lua 5.2

--- tests/run_with_lua.bat
+++ tests/run_with_lua.bat
@@ -3,7 +3,8 @@ cls
 pushd %~dp0
 cd ..\..
 
-SET LUA=luas\lua51\lua.exe
+REM SET LUA=luas\lua51\lua.exe
+SET LUA=luas\lua52\lua.exe
 
 REM %LUA% tsukuyomi\tests\run_all_tests.lua
 %LUA% tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\test_output.log
