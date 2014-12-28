f3c5220c017590a75ee9e4a4ebea7322b4cb734a
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-12-28 00:53:07 -0500

    rename to something shorter

--- tests/run_with_lua.bat
+++ tests/run_with_lua.bat
@@ -7,6 +7,6 @@ REM SET LUA=luas\lua51\lua.exe
 SET LUA=luas\lua52\lua.exe
 
 REM %LUA% tsukuyomi\tests\run_all_tests.lua
-%LUA% tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\test_output.log
+%LUA% tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\output.log
 
 popd
--- tests/run_with_luajit.bat
+++ tests/run_with_luajit.bat
@@ -8,6 +8,6 @@ SET LUA=luas\luajit\luajit.exe
 %LUA% tsukuyomi\thirdparty\bcname.lua 50 51 70 72
 
 REM %LUA% tsukuyomi\tests\run_all_tests.lua
-%LUA% tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\test_output.log
+%LUA% tsukuyomi\tests\run_all_tests.lua > tsukuyomi\tests\output.log
 
 popd
